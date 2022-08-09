454de47c9474e0b64c74f9182c9c980b02ac3c8d
dukejones <dukeofjones@gmail.com>
2022-08-09 11:25:53 -0700

    WIP: reboot docs

--- zomes/deepkey/src/change_rule/zome_call.rs
+++ zomes/deepkey/src/change_rule/zome_call.rs
@@ -3,7 +3,7 @@ use crate::change_rule::entry::ChangeRule;
 
 #[hdk_extern]
 /// ChangeRule can be updated but not created or deleted.
-/// Acutally it can be created but only as part of create_keyset_root.
+/// Actually it can be created but only as part of create_keyset_root.
 fn new_change_rule((old_change_rule, new_change_rule): (HeaderHash, ChangeRule)) -> ExternResult<HeaderHash> {
     update_entry(old_change_rule, new_change_rule)
 }
