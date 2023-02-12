cc8edff1af1932bbe5d4adb27a642a43c42ba5ab
dukejones <dukeofjones@gmail.com>
2023-02-11 20:55:48 -0800

    move to flake.nix

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Flake for Holochain app development";
+
+  inputs = {
+    nixpkgs.follows = "holochain-dev/nixpkgs";
+
+    holochain-dev = {
+      url = "github:holochain/holochain";
+      inputs.versions.url = "github:holochain/holochain/?dir=versions/0_1";
+    };
+  };
+
+  outputs = inputs @ { ... }:
+    inputs.holochain-dev.inputs.flake-parts.lib.mkFlake
+      {
+        inherit inputs;
+      }
+      {
+        systems = builtins.attrNames inputs.holochain-dev.devShells;
+        perSystem =
+          { config
+          , pkgs
+          , system
+          , ...
+          }: {
+            devShells.default = pkgs.mkShell {
+              inputsFrom = [ inputs.holochain-dev.devShells.${system}.holonix ];
+              packages = [ pkgs.nodejs-18_x ];
+            };
+          };
+      };
+}
