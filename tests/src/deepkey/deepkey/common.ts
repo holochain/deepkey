8e534ebe3f7540609803c9b36455cf028034b021
dukejones <dukeofjones@gmail.com>
2023-02-15 19:06:43 -0800

    remove duplicates in typescript

--- tests/src/deepkey/deepkey/common.ts
+++ tests/src/deepkey/deepkey/common.ts
@@ -1,27 +1,5 @@
-export async function sampleJoiningProof(
-  cell: CallableCell,
-  partialJoiningProof = {}
-) {
-  return {
-    ...{
-      keyset_proof: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-      membrane_proof:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    },
-    ...partialJoiningProof,
-  }
-}
-
-export async function createJoiningProof(
-  cell: CallableCell,
-  joiningProof = undefined
-): Promise<Record> {
-  return cell.callZome({
-    zome_name: "deepkey",
-    fn_name: "create_joining_proof",
-    payload: joiningProof || (await sampleJoiningProof(cell)),
-  })
-}
+import { Record } from '@holochain/client';
+import { CallableCell } from "@holochain/tryorama"
 
 export async function sampleJoiningProof(
   cell: CallableCell,
