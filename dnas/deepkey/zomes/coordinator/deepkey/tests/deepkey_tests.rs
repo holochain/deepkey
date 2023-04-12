e602b00b8a916ee30785f4b84b34e5f9ab6a9f13
Harlan Wood <harlan.wood@sharespost.com>
2023-04-12 12:23:42 -1000

    It compiles ;)

--- dnas/deepkey/zomes/coordinator/deepkey/tests/deepkey_tests.rs
+++ dnas/deepkey/zomes/coordinator/deepkey/tests/deepkey_tests.rs
@@ -1,7 +1,6 @@
 #![warn(warnings)]
 
 use hdk::prelude::*;
-// use hdk::prelude::holo_hash::*;
 use holochain::conductor::config::ConductorConfig;
 use holochain::sweettest::{
     SweetCell, SweetConductor, SweetConductorBatch, SweetDnaFile, SweetZome,
@@ -10,8 +9,8 @@ use holochain::test_utils::consistency_10s;
 
 use mews_integrity::*;
 
-const DNA_FILEPATH: &str = "../../workdir/clutter.dna";
-const ZOME_NAME: &str = "mews";
+const DNA_FILEPATH: &str = "../../workdir/deepkey.dna";
+const ZOME_NAME: &str = "deepkey";
 
 pub struct Agent<'a> {
     pub cell: SweetCell,
@@ -47,5 +46,54 @@ async fn trusted_feed_is_based_on_follow_topics() {
         pubkey: ann_cell.agent_pubkey().clone(),
         zome: ann_cell.zome(ZOME_NAME),
     };
-    assert_eq!(recommended_feed.len(), 1);
+    let bob = Agent {
+        cell: bob_cell.clone(),
+        conductor: conductors.get(1).unwrap(),
+        pubkey: bob_cell.agent_pubkey().clone(),
+        zome: bob_cell.zome(ZOME_NAME),
+    };
+
+    // let _: ActionHash = call_zome(&ann, "fn name", SomeInput {}).await;
+
+    let keyGeneration: KeyGeneration = call_zome(
+        &ann,
+        "instantiate_key_generation",
+        bob.pubkey.clone(),
+      );
+
+    // The enum Create option from the KeyRegistration options.
+    let keyRegistration = KeyRegistration {
+        Create: {
+          keyGeneration,
+        },
+      }
+
+      // Register the new key
+      await deepkeyZomeCall(alice)<null>("new_key_registration", keyRegistration)
+
+      consistency_10s([
+          &(ann.cell.clone()),
+          // &(bob.cell.clone()),
+          // &(cat.cell.clone()),
+          ])
+          .await;
+
+    //   expect(sample).toEqual(
+    //     decode((createReadOutput.entry as any).Present.entry) as any
+    //   )
+    // let recommended_feed: Vec<FeedMew> = call_zome(
+    //     &ann,
+    //     "recommended",
+    //     RecommendedInput {
+    //         now: Timestamp::now(),
+    //         oldest_mew_seconds: Some(oldest_mew_seconds),
+    //     },
+    // )
+    // .await;
+
+    // assert_eq!(recommended_feed.len(), 1);
+    // assert_eq!(
+    //     recommended_feed[0].mew.content.as_ref().unwrap().text,
+    //     String::from("NEW #holochain mew")
+    // );
 }
