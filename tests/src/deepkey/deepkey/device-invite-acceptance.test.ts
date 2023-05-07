61b0f0b1841aac8c4e6e48e0d002ee879ecfc42f
Duke Jones <104690+dukejones@users.noreply.github.com>
2023-05-07 13:24:31 -0700

    Update flake, tests run

--- tests/src/deepkey/deepkey/device-invite-acceptance.test.ts
+++ tests/src/deepkey/deepkey/device-invite-acceptance.test.ts
@@ -22,43 +22,47 @@ import { deepkeyZomeCall, isPresent } from "../../utils.js"
 const DNA_PATH = process.cwd() + "/../workdir/deepkey.happ"
 
 test("invite an agent, and have them accept the invite", async (t) => {
-  await runScenario(async (scenario) => {
-    const appSource = { appBundleSource: { path: DNA_PATH } }
+  try {
+    await runScenario(async (scenario) => {
+      const appSource = { appBundleSource: { path: DNA_PATH } }
 
-    const [alice, bob] = await scenario.addPlayersWithApps([
-      appSource,
-      appSource,
-    ])
+      const [alice, bob] = await scenario.addPlayersWithApps([
+        appSource,
+        appSource,
+      ])
 
-    await Promise.all([
-      deepkeyZomeCall(alice)("create_keyset_root"),
-      deepkeyZomeCall(bob)("create_keyset_root"),
-    ])
+      await Promise.all([
+        deepkeyZomeCall(alice)("create_keyset_root"),
+        deepkeyZomeCall(bob)("create_keyset_root"),
+      ])
 
-    await scenario.shareAllAgents()
+      await scenario.shareAllAgents()
 
-    const inviteAcceptance = await deepkeyZomeCall(alice)(
-      "invite_agent",
-      bob.agentPubKey
-    )
+      const inviteAcceptance = await deepkeyZomeCall(alice)(
+        "invite_agent",
+        bob.agentPubKey
+      )
 
-    const acceptanceHash = await deepkeyZomeCall(bob)(
-      "accept_invite",
-      inviteAcceptance
-    )
-    const acceptanceRecord = await deepkeyZomeCall(bob)<Record>(
-      "get_device_invite_acceptance",
-      acceptanceHash
-    )
+      const acceptanceHash = await deepkeyZomeCall(bob)(
+        "accept_invite",
+        inviteAcceptance
+      )
+      const acceptanceRecord = await deepkeyZomeCall(bob)<Record>(
+        "get_device_invite_acceptance",
+        acceptanceHash
+      )
 
-    expect(isPresent(acceptanceRecord.entry)).toBeTruthy()
+      expect(isPresent(acceptanceRecord.entry)).toBeTruthy()
 
-    const acceptanceEntry = (acceptanceRecord.entry as { Present: Entry })
-      .Present.entry
-    const storedAcceptance = decode(acceptanceEntry as Uint8Array)
+      const acceptanceEntry = (acceptanceRecord.entry as { Present: Entry })
+        .Present.entry
+      const storedAcceptance = decode(acceptanceEntry as Uint8Array)
 
-    expect(storedAcceptance).toEqual(inviteAcceptance)
-  })
+      expect(storedAcceptance).toEqual(inviteAcceptance)
+    })
+  } catch (e) {
+    throw e.data.data
+  }
 })
 
 test.skip("create and read DeviceInviteAcceptance", async (t) => {
--- tests/src/deepkey/deepkey/key-registration.test.ts
+++ tests/src/deepkey/deepkey/key-registration.test.ts
@@ -1,3 +1,4 @@
+import { DeviceInviteAcceptance } from "./../../../../ui/src/deepkey/deepkey/types"
 import { describe, expect, test } from "vitest"
 
 import { runScenario, pause } from "@holochain/tryorama"
@@ -25,49 +26,58 @@ type KeyGeneration = {
 type KeyRegistration = {}
 
 test("new_key_registration", async (t) => {
-  await runScenario(async (scenario) => {
-    const appSource = { appBundleSource: { path: DNA_PATH } }
+  try {
+    await runScenario(async (scenario) => {
+      const appSource = { appBundleSource: { path: DNA_PATH } }
 
-    const [alice, bob] = await scenario.addPlayersWithApps([
-      appSource,
-      appSource,
-    ])
+      const [alice, bob] = await scenario.addPlayersWithApps([
+        appSource,
+        appSource,
+      ])
 
-    await Promise.all([
-      deepkeyZomeCall(alice)("create_keyset_root"),
-      deepkeyZomeCall(bob)("create_keyset_root"),
-    ])
+      await Promise.all([
+        deepkeyZomeCall(alice)("create_keyset_root"),
+        deepkeyZomeCall(bob)("create_keyset_root"),
+      ])
 
-    await scenario.shareAllAgents()
+      await scenario.shareAllAgents()
 
-    const newKeyToRegister = await fakeAgentPubKey()
-    // First get the KeyGeneration, with valid signature of the new key from the Deepkey chain agent
-    const keyGeneration = await deepkeyZomeCall(alice)<KeyGeneration>(
-      "instantiate_key_generation",
-      newKeyToRegister
-    )
-    // The enum Create option from the KeyRegistration options.
-    const keyRegistration = {
-      Create: {
-        ...keyGeneration,
-      },
-    }
+      const newKeyToRegister = await fakeAgentPubKey()
+      // First get the KeyGeneration, with valid signature of the new key from the Deepkey chain agent
+      const keyGeneration = await deepkeyZomeCall(alice)<KeyGeneration>(
+        "instantiate_key_generation",
+        newKeyToRegister
+      )
+      // The enum Create option from the KeyRegistration options.
+      const keyRegistration = {
+        Create: {
+          ...keyGeneration,
+        },
+      }
 
-    // Register the new key
-    await deepkeyZomeCall(alice)<null>("new_key_registration", keyRegistration)
+      // Register the new key
+      await deepkeyZomeCall(alice)<null>(
+        "new_key_registration",
+        keyRegistration
+      )
 
-    // Retrieve the KeyRegistration via its KeyAnchor
-    const createdKeyRegistrationRecord = await deepkeyZomeCall(alice)<Record>(
-      "get_key_registration_from_agent_pubkey_key_anchor",
-      newKeyToRegister
-    )
+      // Retrieve the KeyRegistration via its KeyAnchor
+      const createdKeyRegistrationRecord = await deepkeyZomeCall(alice)<Record>(
+        "get_key_registration_from_agent_pubkey_key_anchor",
+        newKeyToRegister
+      )
 
-    expect(isPresent(createdKeyRegistrationRecord.entry)).toBeTruthy()
+      expect(isPresent(createdKeyRegistrationRecord.entry)).toBeTruthy()
 
-    const createdKeyRegistrationEntry = (
-      createdKeyRegistrationRecord.entry as { Present: Entry }
-    ).Present.entry
-    const storedKeyRegistration = decode(createdKeyRegistrationEntry as Uint8Array)
-    expect(storedKeyRegistration).toEqual(keyRegistration)
-  })
+      const createdKeyRegistrationEntry = (
+        createdKeyRegistrationRecord.entry as { Present: Entry }
+      ).Present.entry
+      const storedKeyRegistration = decode(
+        createdKeyRegistrationEntry as Uint8Array
+      )
+      expect(storedKeyRegistration).toEqual(keyRegistration)
+    })
+  } catch (e) {
+    throw e.data.data
+  }
 })
