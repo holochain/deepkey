982bcf7641f6d61543cb284a977895fad041d9de
Duke Jones <104690+dukejones@users.noreply.github.com>
2023-05-07 16:21:35 -0700

    Found it

--- dnas/deepkey/zomes/coordinator/deepkey/src/key_generation.rs
+++ dnas/deepkey/zomes/coordinator/deepkey/src/key_generation.rs
@@ -3,12 +3,15 @@ use hdk::prelude::*;
 #[hdk_extern]
 pub fn instantiate_key_generation(new_key: AgentPubKey) -> ExternResult<KeyGeneration> {
     let chain_key = agent_info()?.agent_latest_pubkey;
+    // !!!!!! This needs to change but it breaks everything
+    // let signature = sign(new_key.clone(), chain_key)?;
     let signature = sign(chain_key, new_key.clone())?;
     Ok(KeyGeneration {
         new_key,
         new_key_signing_of_author: signature,
     })
 }
+
 #[hdk_extern]
 pub fn get_key_generation(key_generation_hash: ActionHash) -> ExternResult<Option<Record>> {
     get(key_generation_hash, GetOptions::default())
