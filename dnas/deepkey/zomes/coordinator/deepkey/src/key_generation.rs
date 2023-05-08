1341dd6a6105e803a419b9602446a86a561a9931
Duke Jones <104690+dukejones@users.noreply.github.com>
2023-05-07 21:41:44 -0700

    Import some key_revocation work

--- dnas/deepkey/zomes/coordinator/deepkey/src/key_generation.rs
+++ dnas/deepkey/zomes/coordinator/deepkey/src/key_generation.rs
@@ -3,7 +3,7 @@ use hdk::prelude::*;
 #[hdk_extern]
 pub fn instantiate_key_generation(new_key: AgentPubKey) -> ExternResult<KeyGeneration> {
     let chain_key = agent_info()?.agent_latest_pubkey;
-    // !!!!!! This needs to change but it breaks everything
+    // !!!!!! This needs to change but it breaks everything, since you can't sign with a pubkey.
     // let signature = sign(new_key.clone(), chain_key)?;
     let signature = sign(chain_key, new_key.clone())?;
     Ok(KeyGeneration {
--- dnas/deepkey/zomes/coordinator/deepkey/src/key_revocation.rs
+++ dnas/deepkey/zomes/coordinator/deepkey/src/key_revocation.rs
@@ -1,33 +1,64 @@
-use hdk::prelude::*;
 use deepkey_integrity::*;
+use hdk::prelude::*;
+
+// // function instantiate_key_revocation creates and returns a KeyRevocation in an ExternResult
+// #[hdk_extern]
+// pub fn instantiate_key_revocation(
+//     prior_key_registration: ActionHash,
+// ) -> ExternResult<KeyRevocation> {
+//     Ok(KeyRevocation {
+//         prior_key_registration,
+//         revocation_authorization: Vec::new(),
+//     })
+// }
+
 #[hdk_extern]
-pub fn create_key_revocation(key_revocation: KeyRevocation) -> ExternResult<Record> {
-    let key_revocation_hash = create_entry(
-        &EntryTypes::KeyRevocation(key_revocation.clone()),
+/// Receives a KeyRevocation, which in the simplest case could be a
+/// prior_key_registration (ActionHash) and an empty Vec
+/// Returns a KeyRevocation with an additional revocation_authorization signed by this agent
+pub fn authorize_key_revocation(key_revocation: KeyRevocation) -> ExternResult<KeyRevocation> {
+    let my_signature = sign(
+        agent_info()?.agent_latest_pubkey,
+        key_revocation.prior_key_registration.clone(),
     )?;
-    let record = get(key_revocation_hash.clone(), GetOptions::default())?
-        .ok_or(
-            wasm_error!(
-                WasmErrorInner::Guest(String::from("Could not find the newly created KeyRevocation"))
-            ),
-        )?;
+    // TODO: find index of my agent's key in authorized_signers
+    // AuthoritySpec authorized_signers: Vec<AgentPubKey>
+
+    let my_authorization = (0, my_signature); // TODO: replace 0 with index of my agent's key in authorized_signers
+    let mut authorizations = key_revocation.revocation_authorization;
+    // authorizations.push(my_authorization);
+    let new_key_revocation = KeyRevocation {
+        prior_key_registration: key_revocation.prior_key_registration,
+        revocation_authorization: authorizations,
+    };
+    Ok(new_key_revocation)
+}
+
+#[hdk_extern]
+pub fn create_key_revocation_record(key_revocation: KeyRevocation) -> ExternResult<Record> {
+    let key_revocation_hash = create_entry(&EntryTypes::KeyRevocation(key_revocation.clone()))?;
+    let record = get(key_revocation_hash.clone(), GetOptions::default())?.ok_or(wasm_error!(
+        WasmErrorInner::Guest(String::from(
+            "Could not find the newly created KeyRevocation"
+        ))
+    ))?;
     Ok(record)
 }
+
 #[hdk_extern]
 pub fn get_key_revocation(
     original_key_revocation_hash: ActionHash,
 ) -> ExternResult<Option<Record>> {
     get_latest_key_revocation(original_key_revocation_hash)
 }
-fn get_latest_key_revocation(
-    key_revocation_hash: ActionHash,
-) -> ExternResult<Option<Record>> {
-    let details = get_details(key_revocation_hash, GetOptions::default())?
-        .ok_or(wasm_error!(WasmErrorInner::Guest("KeyRevocation not found".into())))?;
+fn get_latest_key_revocation(key_revocation_hash: ActionHash) -> ExternResult<Option<Record>> {
+    let details = get_details(key_revocation_hash, GetOptions::default())?.ok_or(wasm_error!(
+        WasmErrorInner::Guest("KeyRevocation not found".into())
+    ))?;
     let record_details = match details {
-        Details::Entry(_) => {
-            Err(wasm_error!(WasmErrorInner::Guest("Malformed details".into())))
-        }
+        Details::Entry(_) => Err(wasm_error!(WasmErrorInner::Guest(
+            "Malformed details".into()
+        ))),
         Details::Record(record_details) => Ok(record_details),
     }?;
     if record_details.deletes.len() > 0 {
@@ -49,11 +80,10 @@ pub fn update_key_revocation(input: UpdateKeyRevocationInput) -> ExternResult<Re
         input.previous_key_revocation_hash,
         &input.updated_key_revocation,
     )?;
-    let record = get(updated_key_revocation_hash.clone(), GetOptions::default())?
-        .ok_or(
-            wasm_error!(
-                WasmErrorInner::Guest(String::from("Could not find the newly updated KeyRevocation"))
-            ),
-        )?;
+    let record = get(updated_key_revocation_hash.clone(), GetOptions::default())?.ok_or(
+        wasm_error!(WasmErrorInner::Guest(String::from(
+            "Could not find the newly updated KeyRevocation"
+        ))),
+    )?;
     Ok(record)
 }
