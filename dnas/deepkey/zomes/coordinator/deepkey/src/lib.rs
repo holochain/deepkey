e6d7e4125908e9f12f377cf84bba31e950291c08
Duke Jones <104690+dukejones@users.noreply.github.com>
2023-09-21 04:00:37 -0700

    query_local_key_info

--- dnas/deepkey/zomes/coordinator/deepkey/src/lib.rs
+++ dnas/deepkey/zomes/coordinator/deepkey/src/lib.rs
@@ -10,6 +10,8 @@ pub mod key_registration;
 pub mod key_revocation;
 pub mod keyset_root;
 pub mod source_of_authority;
+pub mod dna_binding;
+
 use deepkey_integrity::*;
 use hdk::prelude::*;
 use keyset_root::create_keyset_root;
