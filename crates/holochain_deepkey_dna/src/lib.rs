92c88679d1de1277110cbe4c1dbcf88217ab9e88
Michael Dougherty <maackle.d@gmail.com>
2024-04-12 16:36:23 -0700

    Crate stuff

--- crates/holochain_deepkey_dna/src/lib.rs
+++ crates/holochain_deepkey_dna/src/lib.rs
@@ -3,3 +3,7 @@
 /// Get the hard-coded Deepkey DNA provided by this crate.
 /// This can be decoded with `holochain_types::DnaBundle::decode()`
 pub const DEEPKEY_DNA_BUNDLE_BYTES: &[u8] = include_bytes!("deepkey.dna");
+
+pub mod types {
+    pub use deepkey_sdk::*;
+}
