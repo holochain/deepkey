29c7fae0cab64a16d3248abbcae189ca96a20ac1
Matthew Brisebois <matthew@webheroes.ca>
2024-08-20 15:17:33 -0600

    Revert flake.nix

--- flake.nix
+++ flake.nix
@@ -5,37 +5,36 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs }:
-    let
-      system = "x86_64-linux";
-      pkgs = import ./pkgs.nix {
-        pkgs = nixpkgs.legacyPackages.${system};
-        inherit system;
-      };
-    in
-    {
-      devShells.${system} = {
-        default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            holochain_0-4
-            lair-keystore_0-4-5
-            hc_0-4
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import ./pkgs.nix {
+          pkgs = nixpkgs.legacyPackages.${system};
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              holochain_0-4
+              lair-keystore_0-4-5
+              hc_0-4
 
-            rustup
-            cargo
-            rustc
+              rustup
+              cargo
+              rustc
 
-            nodejs_22
-          ];
+              nodejs_22
+            ];
 
-          shellHook = ''
-            export PS1="\[\e[1;32m\](flake-env)\[\e[0m\] \[\e[1;34m\]\u@\h:\w\[\e[0m\]$ "
+            shellHook = ''
+              export PS1="\[\e[1;32m\](flake-env)\[\e[0m\] \[\e[1;34m\]\u@\h:\w\[\e[0m\]$ "
               export CARGO_HOME=$(pwd)/.cargo
               export RUSTUP_HOME=$(pwd)/.rustup
-            rustup default stable
-            rustup target add wasm32-unknown-unknown
-          '';
+              rustup default stable
+              rustup target add wasm32-unknown-unknown
+            '';
         };
-      };
-    };
+      }
+    );
 }
