a4cdaa9eca56716e659a5f5270eab667e65730d8
ThetaSinner <ThetaSinner@users.noreply.github.com>
2025-04-01 12:40:06 +0100

    chore: Use custom Holochain with DPKI enabled

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     holonix = {
-        url = "github:holochain/holonix?ref=support-integrate-k2";
-        inputs.holochain.url  = "github:holochain/holochain?ref=feat/integrate-k2";
+      url = "github:holochain/holonix?ref=support-integrate-k2";
+      inputs.holochain.url = "github:holochain/holochain?ref=feat/integrate-k2";
     };
 
     nixpkgs.follows = "holonix/nixpkgs";
@@ -13,22 +13,34 @@
 
   outputs = inputs@{ flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
     systems = builtins.attrNames inputs.holonix.devShells;
-    perSystem = { inputs', pkgs, ... }: {
-      formatter = pkgs.nixpkgs-fmt;
+    perSystem = { inputs', pkgs, ... }:
+      let
+        # Disable default features and enable wasmer_wamr for a wasm interpreter,
+        # as well as re-enabling tx5 and sqlite-encrypted.
+        cargoExtraArgs = "--features unstable-dpki";
+        # Override arguments passed in to Holochain build with above feature arguments.
+        customHolochain = inputs'.holonix.packages.holochain.override { inherit cargoExtraArgs; };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
 
-      devShells.default = pkgs.mkShell {
-        packages = (with inputs'.holonix.packages; [
-          holochain
-          lair-keystore
-          rust
-        ]) ++ (with pkgs; [
-          nodejs_22
-        ]);
+        devShells.default = pkgs.mkShell {
+          packages = [
+            # Include custom build of Holochain in dev shell.
+            customHolochain
+          ]
+          ++ (with inputs'.holonix.packages; [
+            holochain
+            lair-keystore
+            rust
+          ]) ++ (with pkgs; [
+            nodejs_22
+          ]);
 
-        shellHook = ''
-          export PS1='\[\033[1;34m\][holonix:\w]\$\[\033[0m\] '
-        '';
+          shellHook = ''
+            export PS1='\[\033[1;34m\][holonix:\w]\$\[\033[0m\] '
+          '';
+        };
       };
-    };
   };
 }
